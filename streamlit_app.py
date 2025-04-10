a9d4aa367067524962e614f988d6cd38ceae6bd7
Filo1102 <f.toniolo11@gmail.com>
2025-04-10 11:05:06 +0000

    first

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,23 @@
 import streamlit as st
+from PIL import Image
+import requests
+from io import BytesIO
+from streamlit_extras.image_selector import image_selector
+from streamlit_extras.image_selector import show_selection
+from streamlit_image_coordinates import streamlit_image_coordinates
+from streamlit_gsheets import GSheetsConnection
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+def example():
+    "# Click on the image"
+    last_coordinates = streamlit_image_coordinates( "https://images.pexels.com/photos/45201/kitty-cat-kitten-pet-45201.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+    )
+
+
+    st.write(last_coordinates)
+
+example()
+
+conn = st.connection ("gsheets", type=GSheetsConnection)
+conn.read()
+df=conn.read()
+st.dataframe(df)
